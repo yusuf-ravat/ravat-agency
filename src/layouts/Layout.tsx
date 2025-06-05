ee5fc9a3c152ae5898df8c96991019ef87c8a393
yusuf-ravat <yusufravat009@gmail.com>
2025-06-05 14:53:11 +0530

    SEO changes

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Helmet } from 'react-helmet-async';
 
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <Helmet>
+        <title>Ravat Tech Solution - Your Technology Partner</title>
+        <meta name="description" content="Ravat Tech Solution offers expert technology services including web development, mobile app development, and IT consulting. Get innovative solutions for your business."
+        />
+        <meta name="keywords" content="Ravat Tech Solution, technology services, web development, mobile app development, IT consulting, software development, tech solutions"
+        />
+      </Helmet>
       <Header />
       <main className="flex-grow">
         <Outlet />
@@ -12,4 +20,4 @@ export default function Layout() {
       <Footer />
     </div>
   )
-} 
+}
